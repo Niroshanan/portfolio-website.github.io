faa62e0af27c32757b1dafa2c266e95a41b5a567
sniro <snirosh12@gmail.com>
2023-10-31 22:42:19 +0530

    Email section removed

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection/>
         <ProjectSection/>
-        <EmailSection/>
+        {/* <EmailSection/> */}
       </div>
       <Footer/>
     </main>
