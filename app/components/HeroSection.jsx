0145a992b28bae6412f006b496223efdcf21ebce
sniro <snirosh12@gmail.com>
2023-11-02 12:24:46 +0530

    hero section details updated

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -4,10 +4,9 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 
 export const HeroSection = () => {
-
-  const handleDownloadCv =() =>{
-    const downloadLink = document.createElement('a');
-    downloadLink.href = '/CV/S.Niroshanan_SE_intern.pdf';
+  const handleDownloadCv = () => {
+    const downloadLink = document.createElement("a");
+    downloadLink.href = "/CV/S.Niroshanan_SE_intern.pdf";
     downloadLink.click();
   };
   return (
@@ -27,15 +26,20 @@ export const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            I am a fourth-year student pursuing a BSc Honors degree in Computer
-            Science at the University of Kelaniya. I have a sound knowledge of
-            Java C# and c. basic knowlege in HTML CSS,Javascript
+            I am a fourth-year student at the University of Kelaniya, currently
+            pursuing a BSc Honors degree in Computer Science with a
+            specialization in Data Science. With a solid foundation in
+            programming languages like Java, C#, and C, as well as expertise in
+            web technologies such as HTML, CSS, JavaScript, React, and Next.js.
           </p>
           <div>
             {/* <button className="px-6 py-3 pd-4 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-400 text-white hover:bg-white sm: mb-4 font-bold">
               Hire me
             </button> */}
-            <button onClick={handleDownloadCv} className="px-1 py-1 pd-4 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-400 text-white hover:bg-slate-300 hover:text-black font-bold">
+            <button
+              onClick={handleDownloadCv}
+              className="px-1 py-1 pd-4 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-400 text-white hover:bg-slate-300 hover:text-black font-bold"
+            >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
